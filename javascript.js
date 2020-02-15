7aa57ecb0cc4191e874a50fc512550d09e2c51ed
tamaichihiro <mikumarumutyo@gmail.com>
2020-02-15 14:40:40 +0900

    no message

--- javascript.js
+++ javascript.js
@@ -4,3 +4,23 @@ document.addEventListener('click', function() {
     console.log('クリックされました');
   
 });
+
+myButton.onclick = function() {
+  setUserName();
+}
+
+var myButton = document.querySelector('button');
+var myHeading = document.querySelector('h1');
+
+function setUserName() {
+  var myName = prompt('あなたの好きな食べ物を教えてください');
+  localStorage.setItem('name', myName);
+  myHeading.textContent = 'なにそれおいしいの？' + myName;
+}
+
+if(!localStorage.getItem('name')) {
+  setUserName();
+} else {
+  var storedName = localStorage.getItem('name');
+  myHeading.textContent = '' + storedName;
+}
