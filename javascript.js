7ac996ee2e60bfbf7aa020c90e9257c0490016bb
tamaichihiro <mikumarumutyo@gmail.com>
2020-02-17 18:49:09 +0900

    no message

--- javascript.js
+++ javascript.js
@@ -5,36 +5,27 @@ document.addEventListener('click', function() {
   
 });
 
-<<<<<<< HEAD
-=======
-document.getElementById("button").onclick = function() {
-  // ここに#buttonをクリックしたら発生させる処理を記述する
-};
-
 var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
+var myHeading = document.querySelector('b1');
 
->>>>>>> c4d8879d454a40569f7b899cd0fbbd3133495422
+
 function setUserName() {
   var myName = prompt('あなたの好きな食べ物を教えてください');
   localStorage.setItem('name', myName);
-  myHeading.textContent = 'なにそれおいしいの？' + myName;
+  myHeading.textContent = 'おいしいの？' + myName;
 }
+          myButton.onclick = function() {
+            setUserName();
+          }
+
 
 if(!localStorage.getItem('name')) {
   setUserName();
 } else {
   var storedName = localStorage.getItem('name');
-  myHeading.textContent = '' + storedName;
+  myHeading.textContent ;
 }
-<<<<<<< HEAD
+elem.textContent = text
 
-myButton.onclick = function() {
-  setUserName();
-}
 
-var myButton = document.querySelector('button');
-var myHeading = document.querySelector('h1');
 
-=======
->>>>>>> c4d8879d454a40569f7b899cd0fbbd3133495422
